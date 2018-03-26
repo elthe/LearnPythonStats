7b135bc741c954a53e0c6ddd2b0ae4b205a3a907
elthe <elthe@163.com>
2018-03-26 18:00:39 +0800

    words2 add

--- RM_WORDS.md
+++ RM_WORDS.md
@@ -5,3 +5,7 @@
 ### 根据指定链接，获取文本后，按出现频率生成词云
 ![Image text](images/words_cloud1_result.jpg)
 
+## [words_cloud2.py](words_cloud2.py)
+### 根据最新的财经资讯，根据六大分类，分别生成词云
+![Image text](images/words_cloud2_result.jpg)
+
---
+++ words_cloud2.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+获取即时财经新闻，生成词云
+"""
+
+import jieba.analyse
+
+from wordcloud import WordCloud
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+import tushare as ts
+
+from common import logcm
+from common import wordscm
+
+# 获取即时财经新闻，类型包括国内财经、证券、外汇、期货、港股和美股等新闻信息。
+# 数据更新较快，使用过程中可用定时任务来获取。
+news = ts.get_latest_news(top=150, show_content=True)
+news.dropna()
+logcm.print_obj(news, 'news')
+
+# 信息类型
+classify_list = ['证券', '国内财经', '外汇', '期货', '港股', '美股']
+encoding = 'utf-8'
+
+# 加载停用词列表
+stopwords = wordscm.load_stopwords('./data', 'words_stop1.txt', encoding)
+
+# 导入stopwords
+jieba.analyse.set_stop_words('./data/words_stop1.txt')
+
+# 多子图绘制
+fig, axes = plt.subplots(2, 3, figsize=(15, 8), sharex=True, sharey=True)
+fig.suptitle(u'即时财经新闻-分类关键词云')
+# 设置标题(中文字体)
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+for i in range(2):
+    for j in range(3):
+        index = i * 2 + j
+        classify_name = classify_list[index]
+        news_tmp = news[news.classify == classify_name]
+        logcm.print_obj(news_tmp, 'news_tmp')
+
+        # 标题
+        axes[i][j].set_title('%s (%d 条)' % (classify_name, news_tmp.shape[0]))
+        if news_tmp.size == 0:
+            continue
+
+        # 加载内容URL的内容
+        content = ""
+        for cont in list(news_tmp.content):
+            if cont is not None:
+                content += cont
+
+        # 文字频率排行
+        seg = jieba.analyse.textrank(content, topK=50, withWeight=False, allowPOS=('nt', 'n', 'nv'))
+        cut_text = " ".join(seg)
+        logcm.print_obj(cut_text, 'cut_text')
+
+        # 产生词云
+        color_mask = imread("./data/words_mask_china.png")  # 读取背景图片
+        cloud = WordCloud(
+            # 设置字体，不指定就会出现乱码
+            font_path="./data/words_font_simhei.ttf",
+            # 设置背景色
+            background_color='white',
+            # 词云形状
+            mask=color_mask,
+            # 允许最大词汇
+            max_words=50,
+            # 最大号字体
+            max_font_size=40
+        )
+        word_cloud = cloud.generate(cut_text)
+        # 显示词云图片
+        axes[i][j].imshow(word_cloud)
+
+# 不显示XY轴刻度
+plt.xticks(())
+plt.yticks(())
+# 保存图片
+plt.savefig('images/words_cloud2_result.jpg')
+# 显示
+plt.show()
