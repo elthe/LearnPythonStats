a38a32b354406e2150ca0a12aa61e8417ed3eb8e
elthe <elthe@163.com>
2018-04-18 18:32:21 +0800

    str diff result

--- str_diff.py
+++ str_diff.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-EXCEL数据加载
+字符串对比示例
 """
 
 from common import loadcfgcm
@@ -25,11 +25,29 @@ text2:
 This module provides classes and functions for Comparing sequences.
 including HTML and context and unified diffs.
 difflib document v7.5
+
+add2
 """
 
 text2_lines = text2.splitlines()
 
 d = difflib.Differ()
 diff = d.compare(text1_lines, text2_lines)
+diff_list = list(diff)
 
-logcm.print_info('\n'.join(list(diff)))
+for i in range(len(diff_list)):
+    msg = diff_list[i]
+    msg_next = diff_list[i + 1] if i < len(diff_list) - 1 else ""
+
+    if msg.startswith("+"):
+        if msg_next.startswith("?"):
+            msg += "\n" + msg_next
+        logcm.print_obj(msg, "diff-%d" % i, fg='red', show_header=False)
+
+    elif msg.startswith("-"):
+        if msg_next.startswith("?"):
+            msg += "\n" + msg_next
+        logcm.print_obj(msg, "diff-%d" % i, fg='green')
+
+    elif msg.startswith("?"):
+        continue
