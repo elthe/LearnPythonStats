fd7eefe989689540722b129d6cf71a6b7af5c5ca
elthe <elthe@163.com>
2018-06-01 06:55:11 +0800

    crawl2

---
+++ codegen/codegencm.py
@@ -0,0 +1,27 @@
+from common.classcm import BaseObject
+
+
+class Module(BaseObject):
+    """
+    模块
+    """
+    pass
+
+
+class Service(BaseObject):
+    """
+    接口
+    """
+    req = []
+    resp = []
+    pass
+
+
+class Prop(BaseObject):
+    """
+    属性
+    """
+
+    def __init__(self, name, remark, **kwargs):
+        self.name = name
+        self.remark = remark
--- codegen/jinjia2_sample01.py
+++ codegen/jinjia2_sample01.py
@@ -6,12 +6,9 @@ Jinjia2 使用示例。
 """
 
 from jinja2 import Template
+from common import filecm
 
-tpl_str = """
-{% for i in config %}
-{{i}}
-{% endfor %}
-"""
-
+tpl_str = filecm.read_str(file_name="./template/sample_01.tpl")
 out_str = Template(tpl_str, trim_blocks=True).render(config=[1, 2, 3])
-print(out_str)
+filecm.save_str(out_str, file_name="./output/sample_01.txt")
+#print(out_str)
